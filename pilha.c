7d3a3d7c3222bd3a1983a311c30102c1f96150e6
Gabriel Nunes <gabriellnc@hotmail.com>
2018-12-12 18:15:32 -0200

    teste

---
+++ pilha.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilha.h"
+
+typedef struct no {
+  Token token;
+  struct no *prox;
+} No;
+
+struct pilha {
+  No *primeiro;
+};
+
+Pilha* pilha_criar() {
+	Pilha *p = (Pilha*)malloc(sizeof(Pilha));
+    p->primeiro = NULL;
+    return p;
+}
+
+void pilha_push(Pilha *p, Token t) {
+	No *tmp = (No*)malloc(sizeof(No));
+    tmp->token = t;
+
+    tmp->prox = p->primeiro;
+    p->primeiro = tmp;
+    free(tmp);
+}
+
+Token pilha_pop(Pilha *p) {
+    if(p->primeiro == NULL){
+        
+        return;
+    }
+
+	Token tmpToken = p->primeiro->token;
+    No *tmp = p->primeiro;
+
+    p->primeiro = p->primeiro->prox;
+    free(tmp);
+
+    return(tmpToken);
+}
+
+Token pilha_primeiro(Pilha *p) {
+    if(p->primeiro == NULL){
+        
+        return;
+    }
+    Token tmpToken = p->primeiro->token;
+	return(tmpToken);
+}
+
+int pilha_vazia(Pilha *p) {
+	if (p->primeiro == NULL){
+        return(1);
+    }
+    else{
+        return(0);
+    }
+}
+
+void pilha_destruir(Pilha *p) {
+	No *tmp = p->primeiro;
+    while(tmp != NULL){
+        No *excluir = tmp;
+        tmp = tmp->prox;
+        free(excluir);
+        free(tmp);
+    }
+    free(p);
+}
+
+void pilha_imprimir(Pilha *p) {
+	if(p->primeiro == NULL){
+       
+        return;
+    }
+    No *tmp = p->primeiro;
+    while(tmp != NULL){
+        Token tmpToken = tmp->token;
+        token_imprimir(tmpToken);
+        tmp = tmp->prox;
+    }
+}
+
