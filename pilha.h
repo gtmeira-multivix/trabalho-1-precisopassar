7035bbc876c336af59f84ad6f99081a67866c068
Gabriel Nunes <gabriellnc@hotmail.com>
2018-12-12 17:31:03 -0200

    test

---
+++ pilha.h
@@ -0,0 +1,20 @@
+#define _PILHA_H
+#include "tokenizacao.h"
+
+typedef struct pilha Pilha;
+
+Pilha* pilha_criar();
+
+void pilha_push(Pilha *p, Token t);
+
+Token pilha_pop(Pilha *p);
+
+Token pilha_primeiro(Pilha *p);
+
+int pilha_vazia(Pilha *p);
+
+void pilha_destruir(Pilha *p);
+
+void pilha_imprimir(Pilha *p);
+
+
