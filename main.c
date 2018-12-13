00f8b554d74bd8caa111a23e040c5fa39efcc763
Gabriel Nunes <gabriellnc@hotmail.com>
2018-12-13 13:14:25 -0200

    teste

--- main.c
+++ main.c
@@ -109,8 +109,6 @@ char opcaoMenu[100];
         }
     }
     printf("\n\n\n Resultado do calculo: %.4f\n\n", pilha_pop(pilhaCalc).valor);
-    pilha_destruir(pilhaOp);
-    fila_destruir(filaSaida);
     printf("\n Digite 'S' para sair ou qualquer digito para calcular novamente: ");
 	gets(opcaoMenu);
 	} while ((opcaoMenu[0] != 'S' && opcaoMenu[0] != 's') || strlen(opcaoMenu)>1);
